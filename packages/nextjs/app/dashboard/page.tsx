cb3c90334d86eaa12b92cb720af0281a894bf354
0xyshv <writetoyshv@gmail.com>
2024-06-23 18:23:39 +0530

    chore: add basic UI for dashboard

--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import MyTrades from "~~/components/dashboard/myTrades";
+import TradingAgents from "~~/components/dashboard/tradingAgents";
 import { getMetadata } from "~~/utils/scaffold-stark/getMetadata";
 
 // export const metadata = getMetadata({
@@ -11,8 +12,15 @@ import { getMetadata } from "~~/utils/scaffold-stark/getMetadata";
 const Dashboard: NextPage = () => {
   return (
     <>
-      <div>Dashboard</div>
-      <div className=" mt-8 p-10">
+      <div className="mt-8 px-10">
+        <div className="text-4xl p-2 font-bold">AI Trader</div>
+
+        <div className="shadow-lg p-2 rounded-lg bg-secondary">
+          Portfolio Value :
+        </div>
+      </div>
+      <TradingAgents />
+      <div className="mt-8 px-10">
         <MyTrades />
       </div>
     </>
